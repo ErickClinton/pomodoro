5bf6b5a04a1a4358107815b3a2d1e2f52d71718b
Erick Clinton <erickleite2009321@gmail.com>
2022-10-10 19:37:48 -0300

    Adding esLint,styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import react from 'react'
-
-
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 export function App() {
-
-
   return (
-    <div>dasdas</div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant={'primary'} />
+      <Button variant={'secondary'} />
+      <Button variant={'danger'} />
+      <Button variant={'success'} />
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
-
